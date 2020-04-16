5d40646bf577e3ff50110d130ca05ee67fd473d9
lijun <1179451978@qq.com>
2020-04-16 14:27:11 +0800

    update

---
+++ src/main/java/com/yonyou/cyxdms/framework/bean/dto/base/CYXBaseDTO.java
@@ -0,0 +1,14 @@
+package com.yonyou.cyxdms.framework.bean.dto.base;
+
+import com.yonyou.cyx.framework.bean.dto.base.BaseDTO;
+
+/**
+ * //实现DTO 的父类
+ *
+ * @param
+ * @author zhangxianchao
+ * @return
+ * @since 2018/7/22 0022
+ */
+public abstract class CYXBaseDTO extends BaseDTO {
+}
---
+++ src/main/java/com/yonyou/cyxdms/framework/bean/dto/base/DMSBaseDTO.java
@@ -0,0 +1,13 @@
+package com.yonyou.cyxdms.framework.bean.dto.base;
+
+import com.yonyou.cyx.framework.bean.dto.base.BaseDTO;
+
+/**
+ * 所有DTO 的基类
+ *
+ * @author renwd
+ * @since 2018/12/11
+ */
+public abstract class DMSBaseDTO extends BaseDTO{
+
+}
