1216de1e8e01a5ca40174d041455c250588f9725
zhouxiaowen <00441208@want-want.com>
2021-04-19 11:31:03 +0800

    初始化

--- src/test/java/com/yonyou/cyx/generator/CodeGenerator.java
+++ src/test/java/com/yonyou/cyx/generator/CodeGenerator.java
@@ -5,11 +5,9 @@ import com.yonyou.cyx.util.MyBatisPlusGenerator;
 /**
  * 用于代码生成
  *
- * @author: zhangxianchao
- * @since: 2018/7/28 0028
- * @updateDate:
- * @updateRemark:
- * @version:1.0 Copyright: Copyright (c) 2018
+ * @author zhouxiaowen
+ * @date 2021-04-19 11:27
+ * @version 1.0
  */
 public class CodeGenerator {
     public static void main(String[] args) {
